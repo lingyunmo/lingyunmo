732e6c898c2f9815670e7985ae2c37e359818614
lingyunmo <96475062+lingyunmo@users.noreply.github.com>
2023-10-28 20:06:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,27 +33,55 @@ const readme = `\
 ##
 :pushpin:Now working on
 
+![Spring](https://img.shields.io/badge/Spring-Framework-brightgreen?style=plastic&logo=spring)
+![Spring Boot](https://img.shields.io/badge/Spring%20Boot-Web%20Development-brightgreen?style=plastic&logo=spring)
+![Maven](https://img.shields.io/badge/Maven-Build%20Automation-blue?style=plastic&logo=apache-maven)
+![Android](https://img.shields.io/badge/Android-App%20Development-green?style=plastic&logo=android)
+![Gradle](https://img.shields.io/badge/Gradle-Build%20Tool-green?style=plastic&logo=gradle)
 ![DataStructure](https://img.shields.io/badge/DataStructure-C%2FC%2B%2B-brightgreen?style=plastic)
-![JavaWeb](https://img.shields.io/badge/JavaWeb-Java/HTML/JavaScript-orange?style=plastic)
-![Database](https://img.shields.io/badge/Database-MySQL-blue?style=plastic)
-![Yunzai-Bot](https://img.shields.io/badge/Yunzai--Bot-Node.js-red?style=plastic)
-![YuanShen](https://img.shields.io/badge/Genshin%20Impact-202797524-blueviolet?style=plastic)
+![JavaEE](https://img.shields.io/badge/JavaEE-Java%20Enterprise%20Edition-orange?style=plastic)
+![MySQL](https://img.shields.io/badge/MySQL-Database-blue?style=plastic&logo=mysql)
+![Ubuntu](https://img.shields.io/badge/Ubuntu-Operating%20System-blue?style=plastic&logo=ubuntu)
 
 :penguin:Now in use
 
-![Java](https://img.shields.io/badge/java-green?style=plastic)
-![C](https://img.shields.io/badge/C-green?style=plastic&logo=c&logoColor=white)
 ![MySQL](https://img.shields.io/badge/mysql-blueviolet?style=plastic&logo=mysql&logoColor=white)
 ![GitHub](https://img.shields.io/badge/GitHub-red?style=plastic&logo=github)
 ![MacOS](https://img.shields.io/badge/MacOS-blue?style=plastic&logo=apple)
 ![Win](https://img.shields.io/badge/Windows-blue?style=plastic&logo=windows)
-![Git](https://img.shields.io/badge/Git-yellowgreen?style=plastic&logo=git&logoColor=white)
+![C/C++](https://img.shields.io/badge/C%2FC%2B%2B-Programming-brightgreen?style=plastic&logo=C%2B%2B)
+![Java](https://img.shields.io/badge/Java-Programming-brightgreen?style=plastic&logo=java)
+![C](https://img.shields.io/badge/C-Programming-brightgreen?style=plastic&logo=c)
+![Python](https://img.shields.io/badge/Python-Programming-brightgreen?style=plastic&logo=python)
+![JavaScript](https://img.shields.io/badge/JavaScript-Web%20Development-yellow?style=plastic&logo=javascript)
+![Git](https://img.shields.io/badge/Git-Version%20Control-green?style=plastic&logo=git)
+![GitHub](https://img.shields.io/badge/GitHub-Profile-blue?style=plastic&logo=github)
+![C#](https://img.shields.io/badge/C%23-Programming-brightgreen?style=plastic&logo=c-sharp)
+![Unity](https://img.shields.io/badge/Unity-Game%20Development-orange?style=plastic&logo=unity)
+![Kotlin](https://img.shields.io/badge/Kotlin-Programming-brightgreen?style=plastic&logo=kotlin)
+
+
+
+:cat:Future may use
+
+![React](https://img.shields.io/badge/React-Framework-yellow?style=plastic&logo=react)
+![Django](https://img.shields.io/badge/Django-Framework-brightgreen?style=plastic&logo=django)
+![VS Code](https://img.shields.io/badge/VS%20Code-IDE-blue?style=plastic&logo=visual-studio-code)
+![AWS](https://img.shields.io/badge/AWS-Cloud%20Computing-orange?style=plastic&logo=amazon-aws)
+![Linux](https://img.shields.io/badge/Linux-Operating%20System-blue?style=plastic&logo=linux)
+
 
 :rocket:Devote to
 
-![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=lingyunmo&repo=dataStructureLearning)
+![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=lingyunmo&repo=Declinera-1.16.5-forge )
+![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username=lingyunmo&repo=Lanthanum)
 
-![](https://raw.githubusercontent.com/lingyunmo/lingyunmo/main/assets/github-contribution-grid-snake.svg)
+
+<picture>
+  <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/lingyunmo/lingyunmo/output/github-contribution-grid-snake-dark.svg">
+  <source media="(prefers-color-scheme: light)" srcset="https://raw.githubusercontent.com/lingyunmo/lingyunmo/output/github-contribution-grid-snake.svg">
+  <img alt="github contribution grid snake animation" src="https://raw.githubusercontent.com/lingyunmo/lingyunmo/output/github-contribution-grid-snake.svg">
+</picture>
 
 \
 `
